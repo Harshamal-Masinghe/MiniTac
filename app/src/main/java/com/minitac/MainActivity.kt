fd10fb8a5811a8df9cbd21ef0af7d4d2241cda18
Harsha <it22577306@my.sliit.lk>
2024-04-27 08:31:36 +0530

    Initial commit

---
+++ app/src/main/java/com/minitac/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.minitac
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.minitac.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.playOfflineBtn.setOnClickListener {
+            createOfflineGame()
+        }
+
+    }
+    fun createOfflineGame(){
+        startGame()
+    }
+
+    fun startGame(){
+        startActivity(Intent(this,GameActivity::class.java))
+    }
+}
